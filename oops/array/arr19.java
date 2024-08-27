316b55ff0734f128a6941c4bb6106456ffd33490
mkguptasbraj <156709418+mkguptasbraj@users.noreply.github.com>
2024-08-27 16:32:02 +0530

    jjj

--- oops/array/arr19.java
+++
@@ -1,6 +0,0 @@
-public class arr19 {
-    public static void main(String[] args) {
-        
-    }
-
-}
--- oops/array/arr20.java
+++ oops/array/arr20.java
@@ -1,5 +1,45 @@
 public class arr20 {
+
+    static void printArray(int [] arr){
+        for(int i = 0;i<arr.length;i++){
+           System.out.print(arr[i] + " "); 
+    }
+    System.out.println( );
+    }
+
+    static void swapInArray(int[] arr , int i , int j)
+    
+{
+    int temp = arr[i];
+    arr[i]=arr[j];
+    arr[j] =temp; 
+
+    }
+    static  void reverseArray(int[]arr){
+        int i = 0; int j=arr.length -1;
+
+
+
+        while( i < j){
+ swapInArray(arr, i, j);
+ i++;
+ j--;
+
+    }
+    }
+    
+
+
+
+
+
+
     public static void main(String[] args) {
-        
+        int [] arr ={11,22,33,44,55,66};
+
+         reverseArray(arr);
+            printArray(arr);
+         
+
     }
 }
